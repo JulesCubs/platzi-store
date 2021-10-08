49f04afc998199a491ad60abeb471758fac82c3a
Julian Cubillos <juliandavid25@hotmail.com>
2021-10-07 20:24:55 -0500

    change config travis.yml

--- src/__test__/components/Product.test.js
+++ src/__test__/components/Product.test.js
@@ -1,7 +1,7 @@
 import 'jsdom-global/register';
 import React from "react";
-import { mount, shallow } from "enzyme"
-import ProviderMock from "../../__mocks__/ProviderMock";
+import { mount, shallow } from "enzyme";
+import ProviderMock from '../../__mocks__/ProviderMock';
 import ProductMock from "../../__mocks__/ProductMock";
 import Product from "../../components/Product"
 
